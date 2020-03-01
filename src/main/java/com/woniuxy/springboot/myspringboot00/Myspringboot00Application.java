281b97afd5112162315d2e824ba95d625e8bd46c
abbymjw <abbymjw@outlook.com>
2020-03-01 20:09:28 +0800

    inserted "01"

--- src/main/java/com/woniuxy/springboot/myspringboot00/Myspringboot00Application.java
+++ src/main/java/com/woniuxy/springboot/myspringboot00/Myspringboot00Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Myspringboot00Application {
 
 	public static void main(String[] args) {
+		System.out.println("01");
 		SpringApplication.run(Myspringboot00Application.class, args);
 	}
 
