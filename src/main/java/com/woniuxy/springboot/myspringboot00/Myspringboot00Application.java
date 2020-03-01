f7a0dd37294a73d1105e8309a64d9d4973d41d5f
abbymjw <abbymjw@outlook.com>
2020-03-01 19:51:31 +0800

    init project

---
+++ src/main/java/com/woniuxy/springboot/myspringboot00/Myspringboot00Application.java
@@ -0,0 +1,13 @@
+package com.woniuxy.springboot.myspringboot00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspringboot00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspringboot00Application.class, args);
+	}
+
+}
